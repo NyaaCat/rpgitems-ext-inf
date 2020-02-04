ba7263cfb0b7889bffc9f2b4926bd13a92d7f5d4
ReinWD <ReinWDD@gmail.com>
2020-02-04 20:19:09 +0800

    add upper bound for rage

--- src/main/java/moe/reinwd/inf/power/cond/RageCondition.java
+++ src/main/java/moe/reinwd/inf/power/cond/RageCondition.java
@@ -2,12 +2,14 @@ package moe.reinwd.inf.power.cond;
 
 import cat.nyaa.infiniteinfernal.InfPlugin;
 import cat.nyaa.infiniteinfernal.api.InfVarApi;
-import cat.nyaa.infiniteinfernal.ui.impl.VarMana;
 import cat.nyaa.infiniteinfernal.ui.impl.VarRage;
 import moe.reinwd.inf.InfExtentionPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import think.rpgitems.power.*;
+import think.rpgitems.power.Meta;
+import think.rpgitems.power.PowerResult;
+import think.rpgitems.power.Property;
+import think.rpgitems.power.PropertyHolder;
 
 import java.util.Map;
 
@@ -16,6 +18,9 @@ public class RageCondition extends InfCondition<Double> {
     @Property
     public double rage = 0;
 
+    @Property
+    public double rageUpperBound = -1;
+
     @Override
     public PowerResult<Double> check(Player player, ItemStack stack, Map<PropertyHolder, PowerResult<?>> context) {
         InfPlugin plugin = InfExtentionPlugin.getPlugin(InfPlugin.class);
@@ -23,7 +28,7 @@ public class RageCondition extends InfCondition<Double> {
             InfVarApi varApi = plugin.getVarApi();
             VarRage rage = varApi.getRage(player);
             Double value = rage.getValue();
-            if (value >= this.rage) {
+            if (value >= this.rage && (rageUpperBound < 0 || value <= rageUpperBound)) {
                 return PowerResult.ok(value);
             } else {
                 return PowerResult.fail(value);
